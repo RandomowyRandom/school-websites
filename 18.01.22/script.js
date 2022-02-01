ceec4e776632dabe21949363fc3ddd5a4e8a1a05
RandomowyRandom <domi20100501@gmail.com>
2022-02-01 16:37:24 +0100

    Trojkat

--- 18.01.22/script.js
+++ 18.01.22/script.js
@@ -1,46 +1,57 @@
-const rokInput = document.getElementById('rok');
-const rokButton = document.getElementById('check');
-const rokOutput = document.getElementById('rokOutput');
+const rokInput = document.getElementById("rok");
+const rokButton = document.getElementById("check");
+const rokOutput = document.getElementById("rokOutput");
 
 // check if the year is a leap year
 function isLeapYear(year) {
-    return (year % 4 == 0 && year % 100 != 0) || year % 400 == 0;
+  return (year % 4 == 0 && year % 100 != 0) || year % 400 == 0;
 }
 
-rokButton.addEventListener('click', e => {
-    e.preventDefault();
-    const rok = rokInput.value;
-    if (isLeapYear(rok)) {
-        rokOutput.innerHTML = `${rok} jest przestępny`
-    } else {
-        rokOutput.innerHTML = `${rok} nie jest przestępny`
-    }
+rokButton.addEventListener("click", (e) => {
+  e.preventDefault();
+  const rok = rokInput.value;
+  if (isLeapYear(rok)) {
+    rokOutput.innerHTML = `${rok} jest przestępny`;
+  } else {
+    rokOutput.innerHTML = `${rok} nie jest przestępny`;
+  }
 });
 
-const num1 = document.getElementById('num1');
-const num2 = document.getElementById('num2');
-const num3 = document.getElementById('num3');
+const num1 = document.getElementById("num1");
+const num2 = document.getElementById("num2");
+const num3 = document.getElementById("num3");
 
-const triangleButton = document.getElementById('check2');
-const triangleOutput = document.getElementById('triangleOutput');
+const triangleButton = document.getElementById("check2");
+const triangleOutput = document.getElementById("triangleOutput");
 
 // check if the triangle exists
 function isTriangle(a, b, c) {
-    return (a + b > c) && (b + c > a) && (a + c > b);
+  return a + b > c && b + c > a && a + c > b;
 }
 
-triangleButton.addEventListener('click', e => {
-    e.preventDefault();
-    const a = num1.value;
-    const b = num2.value;
-    const c = num3.value;
-    if (isTriangle(a, b, c)) {
-        triangleOutput.innerHTML = `${a}, ${b}, ${c} to trójkąt`
-    } else {
-        triangleOutput.innerHTML = `${a}, ${b}, ${c} nie jest trójkąt`
-    }
-})
+triangleButton.addEventListener("click", (e) => {
+  e.preventDefault();
 
+  // check if number is an integer
+  if (
+    !Number.isInteger(Number(num1.value)) ||
+    !Number.isInteger(Number(num2.value)) ||
+    !Number.isInteger(Number(num3.value))
+  ) {
+    triangleOutput.innerHTML = "Wprowadź liczby całkowite";
+    return;
+  }
+
+  const a = parseInt(num1.value);
+  const b = parseInt(num2.value);
+  const c = parseInt(num3.value);
+
+  if (isTriangle(a, b, c)) {
+    triangleOutput.innerHTML = `${a}, ${b}, ${c} to trójkąt`;
+  } else {
+    triangleOutput.innerHTML = `${a}, ${b}, ${c} nie jest trójkąt`;
+  }
+});
 
 // 1. napisz program ktory wczyta jako parametr wejsciowy funkcji aktualny rok. na stronie wypisze czy dany rok jest przestepny czy nie
 
